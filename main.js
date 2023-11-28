5d72eacf2f822d74a4082252c2f1db24253885ff
Youss0203 <youssefhalibi003@gmail.com>
2023-11-28 16:18:32 +0100

    inizio funzione per creare l'elemento

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+/*
+Descrizione 
+Ricreiamo un feed social aggiungendo al layout di base fornito, 
+il nostro script JS in cui:
+Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. 
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali 
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. 
+Unsplash (https://unsplash.it/300/300?image=<id>)
+Milestone 2 - 
+Prendendo come riferimento il layout di esempio presente nell'html, 
+stampiamo i post del nostro feed.
+Milestone 3 - 
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e 
+incrementiamo il counter dei likes relativo. Salviamo in un secondo 
+array gli id dei post ai quali abbiamo messo il like.
+ */
+
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +81,51 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+const newPost = document.getElementById("container")
+
+
+
+/* FUNZIONI */
+
+function generateNewPost(){
+
+    innerHTML +=`
+
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">Phil Mangione</div>/* qua metto l'autore */
+                        <div class="post-meta__time">4 mesi fa</div> /* qua metto la data di creazione */
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div> /* qua metto il contenuto */
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=171" alt="">/* qua metto l'immagine */
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>/* qua metto i like */
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone /*qua metto il counter*/
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `
+
+}
+
+
